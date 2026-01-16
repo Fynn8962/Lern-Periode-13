0a55a6ffae8512ccf585c9808dc2c5cb3a1eba00
Fynn Huber <fynn.huber@icloud.com>
2026-01-16 15:56:39 +0100

    adding board and handle clicks

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
+// components for a sqaure
+function Square() {
+  
+  // value - stores the Value 
+  // setValue - function to change the value 
+  // null - passed a initial value for this state variable
+  const [value, setValue] = useState(null);
+  
+
+  // handle click of the Button
+  function handleClick() {
+    setValue('X');
+  }
+
+  // Loading the Button in HTML
   return (
-    <h1>Hello World</h1>
-  )
+    <button className="square" onClick={handleClick}>{value}</button>
+  );
 }
 
-export default App
+// using th Square component in the Board function 
+export default function Board() {
+  return (
+    <>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    </>
+  );
+}
